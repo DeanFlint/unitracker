c1d18e7b2ff042091577c21cacebbc1650f62476
Dean Flint <dean.p.flint@gmail.com>
2018-05-13 19:54:33 +0000

    Bugs model updated to set defaults on status and votes

--- bugs/models.py
+++ bugs/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Bug(models.Model):
     name = models.CharField(max_length=75)
     desc = models.TextField(max_length=500)
-    status = models.CharField(max_length=11)
-    votes = models.IntegerField()
+    status = models.CharField(max_length=15, default="To Do")
+    votes = models.IntegerField(null=False, default=0)
     
     def __str__(self):
         return self.name
