31521d36062da584307a2e8d63db5f74a955dfe9
Dean Flint <dean.p.flint@gmail.com>
2018-05-13 19:55:06 +0000

    Bugs urls updated to include view_bug and create_bug

--- bugs/urls.py
+++ bugs/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-from .views import view_bugs
-# from .views import view_cart, add_to_cart, adjust_cart
+from .views import view_bugs, view_bug, create_bug
 
 urlpatterns = [
-    url(r'^$', view_bugs, name='view_bugs')
+    url(r'^$', view_bugs, name='view_bugs'),
+    url(r'^(?P<pk>\d+)/$', view_bug, name='view_bug'),
+    url(r'^new/$', create_bug, name='create_bug'),
     ]
