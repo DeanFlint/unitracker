7f6e9689599ad6177414d0203e9923b5a005b1f1
Dean Flint <dean.p.flint@gmail.com>
2018-05-20 19:27:36 +0000

    Small changes

---
+++ bugs/.~c9_invoke_W71nXT.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Bug, Comment
+
+class CreateBugForm(forms.ModelForm):
+    class Meta:
+        model = Bug
+        fields = ('name', 'desc')
+        
+class CreateCommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('comment',)
+        
+class FilterView(forms.Form):
+    ORDER_BY_CHOICES = [
+        ('name_az', 'Name: A - Z'),
+        ('name_za', 'Name: Z - A'),
+        ('status_az', 'Status: A - Z'),
+        ('status_za', 'Status: Z - A'),
+        ('votes_lowtohigh', 'Votes: Low to High'),
+        ('votes_hightolow', 'Votes: High to Low')
+    ]
+    order_by = forms.ChoiceField(choices=ORDER_BY_CHOICES, labe)
