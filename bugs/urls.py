0f01210e85e4218e604522f80f307938573f54d6
Dean Flint <dean.p.flint@gmail.com>
2018-05-20 19:23:27 +0000

    Create bug, Add comment urls added

--- bugs/urls.py
+++ bugs/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
-from .views import view_bugs, view_bug, create_bug
+from .views import view_bugs, view_bug, create_bug, add_comment
 
 urlpatterns = [
     url(r'^$', view_bugs, name='view_bugs'),
     url(r'^(?P<pk>\d+)/$', view_bug, name='view_bug'),
     url(r'^new/$', create_bug, name='create_bug'),
+    url(r'^(?P<pk>\d+)/add_comment/$', add_comment, name='add_comment'),
     ]
