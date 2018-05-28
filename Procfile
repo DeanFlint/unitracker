1a0254ae8fd71980129944ed1dca23b99414bd47
Dean Flint <dean.p.flint@gmail.com>
2018-05-28 19:13:13 +0000

    Heroku setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn unitracker.wsgi:application
--- unitracker/settings.py
+++ unitracker/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import env
-# import dj_database_url
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), 'unitracker.herokuapp.com']
 
 
 # Application definition
@@ -85,12 +85,17 @@ WSGI_APPLICATION = 'unitracker.wsgi.application'
 #     DATABASES = { 'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
 # else:
 #     print("Database URL not found, using SQLite instead")
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+if "DATABASE_URL" in os.environ:
+    DATABASES = { 'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+else:
+    print("Database URL not found, using SQLite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
