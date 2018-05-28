ce0f3a416b311ba80dd3eb0fb26e87b89e48a342
Dean Flint <dean.p.flint@gmail.com>
2018-05-28 18:18:53 +0000

    donate functionality and style/layout added

--- donate/models.py
+++ donate/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Donate(models.Model):
+    user = models.ForeignKey(User, null=True)
+    donation = models.DecimalField(max_digits=6, decimal_places=2)
--- donate/views.py
+++ donate/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import DonationForm, DonateValueForm
+from .models import Donate
+from django.conf import settings
+import stripe
 
 # Create your views here.
+stripe.api_key = settings.STRIPE_SECRET
+
+@login_required()
+def donate(request):
+    if request.method=="POST":
+        donation_form = DonationForm(request.POST)
+        donate_value_form = DonateValueForm(request.POST)
+        
+        if donate_value_form.is_valid() and donation_form.is_valid():
+            donation_value = donate_value_form.save(commit=False)
+            user = request.user
+            donation_value.user = user
+            total = donation_value.donation
+            donation_value.save()
+            
+            try:
+                customer = stripe.Charge.create(
+                    amount = int(total * 100),
+                    currency = "EUR",
+                    description = request.user.username,
+                    card = donation_form.cleaned_data['stripe_id'],
+                )
+            except stripe.error.CardError:
+                messages.error(request, "Your card was declined!")
+                
+            if customer.paid:
+                messages.error(request, "You have successfully donate, thank you!")
+                return render(request, "index.html")
+            else:
+                messages.error(request, "Unable to take payment")
+        
+        else:
+            print(donation_form.errors)
+            messages.error(request, "We were unable to take a payment with that card!")
+    else:
+        donation_form = DonationForm()
+        donate_value_form = DonateValueForm
+        
+    return render(request, "donate.html", {'donation_form': donation_form, 'donate_value_form': donate_value_form, 'publishable': settings.STRIPE_PUBLISHABLE})
